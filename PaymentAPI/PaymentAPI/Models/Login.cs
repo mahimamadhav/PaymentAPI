f2eca6d4ffb0754ad249fef4dfd26dbecc078c7b
ashwini <ashwini@DESKTOP-KONA5K5>
2022-06-08 00:55:32 +0530

    Add project files.

---
+++ PaymentAPI/PaymentAPI/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentAPI.Models
+{
+    public class Login
+    {
+
+        public string uname { get; set; }
+        public string pwd { get; set; }
+    }
+}
---
+++ PaymentAPI/PaymentAPI/Models/LoginContextClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+namespace PaymentAPI.Models
+{
+        public class LoginContextClass : DbContext
+        {
+            public LoginContextClass(DbContextOptions<LoginContextClass> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Login> PaymentDetails { get; set; }
+        }
+}
